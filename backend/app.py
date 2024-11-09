10962c063f646bfbe908d687682cc42b1768c503
David <1697252093@qq.com>
2024-11-09 19:09:05 +0800

    update the path problem

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_restful import Api
-from config import Config
-from model import db, db_drop_and_create_all
-from run import bp
+from backend.config import Config
+from backend.model import db, db_drop_and_create_all
+from backend.run import bp
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.register_blueprint(bp)
 app.config.from_object(Config)
 db.init_app(app)
+migrate = Migrate(app, db)
 db_drop_and_create_all(app)
 
 
